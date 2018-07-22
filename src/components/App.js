9f823f255a39c78184c803fbe511a3aede496b28
hualf1995 <hualf2016@gmail.com>
2018-07-22 17:52:05 +0800

    topnavbar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {TopNavBar} from './TopNavBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TopNavBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
